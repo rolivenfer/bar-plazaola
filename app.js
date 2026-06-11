4da06c01fda24f3438ffd0f1c29a2c1e85341171
rolivenfer <rolivenfer@gmail.com>
2026-06-11 19:29:02 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+const accordionItems =
+document.querySelectorAll(".accordion-item");
+
+accordionItems.forEach(item => {
+
+  const header =
+  item.querySelector(".accordion-header");
+
+  header.addEventListener("click", () => {
+
+    item.classList.toggle("active");
+
+  });
+
+});
